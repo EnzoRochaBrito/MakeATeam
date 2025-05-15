05631a8e9632e9021baab7c211482e2f1ae936fa
EnzoRochaBrito <enzo1brich@gmail.com>
2025-05-15 16:50:04 -0300

    chore: adicionando paginas no route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: "open",
+        path: "landing",
+        loadComponent: () => import('./page/landing/landing.component').then(m => m.LandingComponent)
+    },
+    {
+        path: "register",
+        loadComponent: () => import('./page/register/register.component').then(m => m.RegisterComponent)
+    },
+    {
+        path: "login",
+        loadComponent: () => import('./page/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: "open-projects",
         loadComponent: () => import('./page/open-projects/open-projects.component').then(m => m.OpenProjectsComponent)
     },
     {
-        path: "landing",
-        loadComponent: () => import('./page/landing/landing.component').then(m => m.LandingComponent)
+        path: "create",
+        loadComponent: () => import('./page/create-project/create-project.component').then(m => m.CreateProjectComponent)
     },
     {
         path: "**",
