d9b2a223b3c90fffd2ff949a301b167df9c607d0
EnzoRochaBrito <enzo1brich@gmail.com>
2025-05-15 16:45:43 -0300

    feat: pagina login

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthTemplateComponent } from '../../template/auth/auth.component';
+import { CustomInputComponent } from '../../components/custom-input/custom-input.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [AuthTemplateComponent, CustomInputComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  title: string = "Login"
 
+  loginForm =  new FormGroup({
+    email:     new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.minLength(6), Validators.required]),
+  })
 }
