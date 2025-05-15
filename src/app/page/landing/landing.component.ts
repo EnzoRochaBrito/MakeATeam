746996b92affe0f6be31941754a2aa5dec6b1526
EnzoRochaBrito <enzo1brich@gmail.com>
2025-05-15 16:47:45 -0300

    chore: dando funcionalidade a alguns componentes da pagina

--- src/app/page/landing/landing.component.ts
+++ src/app/page/landing/landing.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AltHeaderComponent } from '../../components/alt-header/alt-header.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterLink, AltHeaderComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
