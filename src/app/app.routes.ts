e4c21a204674ef83bbdca6f51bede74e382467db
enzo <enzo1brich@gmail.com>
2025-05-15 00:50:34 -0300

    feat: landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: "**",
-        redirectTo: "open"
-    },
-    {
         path: "open",
         loadComponent: () => import('./page/open-projects/open-projects.component').then(m => m.OpenProjectsComponent)
+    },
+    {
+        path: "landing",
+        loadComponent: () => import('./page/landing/landing.component').then(m => m.LandingComponent)
+    },
+    {
+        path: "**",
+        redirectTo: "landing",
+        pathMatch: 'full'
     }
 ];
