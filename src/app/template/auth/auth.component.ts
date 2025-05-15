dc46ed45ad48032ecd2d55ef6bfa9fe31d94484d
EnzoRochaBrito <enzo1brich@gmail.com>
2025-05-15 16:46:29 -0300

    feat: template pagina login

--- src/app/template/auth/auth.component.ts
+++ src/app/template/auth/auth.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { AltHeaderComponent } from '../../components/alt-header/alt-header.component';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [FooterComponent, AltHeaderComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
-export class AuthComponent {
-
+export class AuthTemplateComponent {
+ @Input() title!: string;
 }
