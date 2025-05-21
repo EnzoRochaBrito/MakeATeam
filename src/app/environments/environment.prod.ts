e36e306d5d56da12e9f158b74fab606c22d1ad44
enzo <enzo1brich@gmail.com>
2025-05-20 22:13:03 -0300

    prod file

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: 'NG_APP_FIREBASE_API_KEY',
+      authDomain: 'NG_APP_FIREBASE_AUTH_DOMAIN',
+      projectId: 'NG_APP_FIREBASE_PROJECT_ID',
+      storageBucket: 'NG_APP_FIREBASE_STORAGE_BUCKET',
+      messagingSenderId: 'NG_APP_FIREBASE_MSG_SENDER_ID',
+      appId: 'NG_APP_FIREBASE_APP_ID'
+    }
+  };
